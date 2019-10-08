2ae80a0df05383b3b7e034893e1124a4bbb14101
matsumoto <matsumoto.k017a1377@gmail.com>
2019-10-08 12:59:44 +0900

    不要なクラスを削除

--- DirectX11/Application/Source/GameObject/Floor.cpp
+++
@@ -1,8 +0,0 @@
-#include "Floor.h"
-
-using namespace Framework;
-
-Floor::Floor(const Utility::Transform& transform, IMainSceneMediator& mediator)
-    :GameObject3D(transform, mediator, Define::ModelType::Plane) {}
-
-Floor::~Floor() {}
--- DirectX11/Application/Source/GameObject/Weapon/PlayerWeapon.cpp
+++
@@ -1,8 +0,0 @@
-#include "PlayerWeapon.h"
-
-using namespace Framework;
-
-PlayerWeapon::PlayerWeapon(const Utility::Transform& transform, IMainSceneMediator& mediator)
-    :GameObject(transform, mediator) {}
-
-PlayerWeapon::~PlayerWeapon() {}
